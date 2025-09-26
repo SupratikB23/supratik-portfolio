5d9f9bf63b180dc87526bda60bc1964f5cf5e61b
Supratik Bhowal <supratikbhowal23@gmail.com>
2025-09-26 13:07:19 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/supratik-portfolio/",
+  base: mode === "production" ? "/supratik-portfolio/" : "/",
   server: {
     host: "::",
     port: 8080,
